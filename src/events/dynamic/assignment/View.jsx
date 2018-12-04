9acecc87bfbd6ec2cba5940fc0a9e948b244d54f
Josh Birdwell <jbirdwell34@gmail.com>
2018-12-04 12:07:15 -0600

    Timed Assignments

--- src/events/dynamic/assignment/View.jsx
+++ src/events/dynamic/assignment/View.jsx
@@ -1,31 +1,64 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { DateTime } from '@nti/web-commons';
+import { DateTime, List } from '@nti/web-commons';
+import cx from 'classnames';
 
-import Registry from '../../Registry';
 import { Icon } from '../../../calendar/components';
 import { Event } from '../../components';
+import Registry from '../../Registry';
 
-export default
 @Registry.register('application/vnd.nextthought.assessment.assignmentcalendarevent')
-class AssignmentEvent extends React.Component {
+export default class AssignmentEvent extends React.Component {
 	static propTypes = {
 		item: PropTypes.shape({
 			title: PropTypes.string,
-			dueDate: PropTypes.string
+			dueDate: PropTypes.string,
+			assignment: PropTypes.shape({
+				getDueDate: PropTypes.func.isRequired,
+				totalPoints: PropTypes.number
+			}).isRequired
 		})
 	}
+
+	renderTimedAssignment () {
+		const { item: { assignment } } = this.props;
+		return (
+			<div className="timed-assignment">
+				<div className="timed-assignment-length">
+					{DateTime.getShortNaturalDuration(assignment.getMaximumTimeAllowed())}
+				</div>
+				<div className="timed-assignment-label">
+					Time Limit
+				</div>
+			</div>
+		);
+	}
+
 	render () {
-		const { item: { title, dueDate }} = this.props;
+		const { item: { title, assignment }} = this.props;
+		const dueDate = assignment && assignment.getDueDate();
+		const { totalPoints, isTimed } = assignment;
 
 		return (
-			<Event.Layout className="assignment-event">
+			<Event.Layout className={cx('assignment-event', { isTimed })}>
 				<Icon />
-				<div className="assignment-meta">
-					<div className="assignment-title">{title}</div>
-					<div className="assignment-due">
-						Due {DateTime.format(new Date(dueDate), 'h:mm A z')}
+				<div className="assignment-content">
+					<div className="assignment-meta">
+						<div className="assignment-title">{title}</div>
+						<List.SeparatedInline className="assignment-subtitle">
+							{dueDate && (
+								<div className="assignment-due">
+									Due {DateTime.format(new Date(dueDate), 'h:mm A z')}
+								</div>
+							)}
+							{totalPoints && (
+								<div className="assignment-points">
+									{totalPoints} PTS.
+								</div>
+							)}
+						</List.SeparatedInline>
 					</div>
+					{isTimed && this.renderTimedAssignment()}
 				</div>
 			</Event.Layout>
 		);
