d226b401dd1c8587c4d59a8536456b171fc12705
Ray Hatfield <ray.hatfield@gmail.com>
2021-06-30 17:21:41 -0500

    NTI-10811 - display issues in bulk check-in completion view; paint

--- src/events/check-in/bulk-upload/Complete.jsx
+++ src/events/check-in/bulk-upload/Complete.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 
-const Container = styled.div`
+import { Box } from '../parts/Containers';
+import { Table as CommonsTable } from '../parts/Table';
+import { TableCellText } from '../columns/shared';
+import { Title, SubTitle } from '../parts/Text';
+
+const Container = styled(Box)`
 	min-height: 350px;
 	display: flex;
 	flex-direction: column;
 	justify-content: center;
-	align-items: center;
 
 	> * {
 		margin: 1rem;
@@ -18,9 +22,41 @@ export default function BulkUploadComplete({
 }) {
 	return (
 		<Container>
-			<div>{items.length} attendees checked in.</div>
-			{issues.length && <div>Encountered {issues.length} issues.</div>}
+			<Title>{items.length} attendees checked in.</Title>
+			<Issues issues={issues} />
 			<button onClick={returnView}>Return</button>
 		</Container>
+	);
+}
+
+const column = (prop, name = prop) => {
+	const Cmp = ({ item: { [prop]: value } }) => (
+		<TableCellText>{value}</TableCellText>
+	);
+	Cmp.Name = name;
+	return Cmp;
+};
+
+const LineColumn = column('line');
+const MessageColumn = column('message');
+
+const Table = styled(CommonsTable)`
+	&& {
+		table-layout: auto;
+	}
+`;
+
+function Issues({ issues, limit = 15 }) {
+	return !issues?.length ? null : (
+		<div>
+			<Title>Encountered {issues.length} issues:</Title>
+			<Table
+				items={issues.slice(0, limit)}
+				columns={[LineColumn, MessageColumn]}
+			/>
+			{issues.length > limit && (
+				<SubTitle>…and {issues.length - limit} more</SubTitle>
+			)}
+		</div>
 	);
 }
