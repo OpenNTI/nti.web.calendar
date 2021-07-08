b5d8f5e9951a1b8e46ee280da2abf51a42b2436a
Jonathan Grimes <jsg2021@gmail.com>
2021-07-08 12:59:16 -0500

    stub checked in banner

---
+++ src/events/Registration.jsx
@@ -0,0 +1,52 @@
+import React from 'react';
+
+import { DateTime, Icons, Text } from '@nti/web-commons';
+import { scoped } from '@nti/lib-locale';
+
+const t = scoped('calendar.events.check-in', {
+	'checked-in-at': 'Checked-In %(date)s',
+});
+
+//#region 🎨
+
+const Box = styled.div`
+	height: 40px;
+	background: linear-gradient(180deg, #dceedd 0%, #e9f6ea 19.04%, #fff 100%);
+	box-shadow: inset 0 -1px 0 0 #dfdfdf;
+	color: var(--secondary-green);
+	padding: 0 40px;
+	display: flex;
+	flex-direction: row;
+	align-items: center;
+	justify-items: flex-start;
+
+	@media (--respond-to-handhelds) {
+		padding: 0 16px;
+	}
+`;
+
+const Label = styled(Text.Label).attrs({ getString: t })`
+	text-transform: none;
+`;
+
+//#endregion
+
+export function Registration({ event }) {
+	const date = event?.getRegistrationTime();
+	return (
+		event?.getRegistrationTime() && (
+			<Box>
+				<Icons.Check />
+				<Label
+					localeKey="checked-in-at"
+					with={{
+						date: DateTime.format(
+							date,
+							DateTime.WEEKDAY_MONTH_NAME_DAY_AT_TIME
+						),
+					}}
+				/>
+			</Box>
+		)
+	);
+}
--- src/events/View.jsx
+++ src/events/View.jsx
@@ -4,6 +4,7 @@ import PropTypes from 'prop-types';
 import { isFlag } from '@nti/web-client';
 
 import Editor from './editor/EventEditor';
+import { Registration } from './Registration';
 
 const CheckIn = React.lazy(() => import('./check-in/View'));
 
@@ -24,6 +25,7 @@ export function View(props) {
 	const Viewer = showCheckIn ? CheckIn : Editor;
 	return (
 		<Suspense fallback={<div />}>
+			<Registration {...props} />
 			<Viewer {...props} />
 		</Suspense>
 	);
