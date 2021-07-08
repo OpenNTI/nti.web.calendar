0381c1b5516d33d096520f685aa26d943b042ff9
Jonathan Grimes <jsg2021@gmail.com>
2021-07-08 16:43:28 -0500

    NTI-10831 - update some paint

--- src/events/editor/Header.jsx
+++ src/events/editor/Header.jsx
@@ -1,75 +1,86 @@
 import React from 'react';
+import cx from 'classnames';
 
 import { DateTime, Input, Text } from '@nti/web-commons';
 import { ImageUpload } from '@nti/web-whiteboard';
 
 import t from './strings';
 
-export function Header(props) {
-	return (
-		<div className="header-info">
-			<DateInfo date={props.startDate} />
-			<EventInfo {...props} />
-		</div>
-	);
-}
-
-function EventInfo({
+export function Header({
+	event,
 	startDate,
 	title,
 	description,
 	img,
-	readOnly: readOnly,
+	readOnly,
 	onTitleChange,
 	onDescriptionChange,
 	onImageChange,
 }) {
+	const suppressDuplicateInfoFromCustomHeader =
+		event.hasLink('list-attendance') && readOnly;
 	return (
-		<div className="event-info">
-			<div className="title">
-				{readOnly ? (
-					<Text.Base linkify>{title}</Text.Base>
-				) : (
-					<Input.Text
-						placeholder={t('eventTitle')}
-						value={title}
-						onChange={onTitleChange}
-						maxLength="140"
+		<div
+			className={cx('header-info', {
+				padded: !suppressDuplicateInfoFromCustomHeader,
+			})}
+		>
+			<DateInfo
+				date={startDate}
+				hidden={suppressDuplicateInfoFromCustomHeader}
+			/>
+			<div className="event-info">
+				<div
+					className="title"
+					hidden={suppressDuplicateInfoFromCustomHeader}
+				>
+					{readOnly ? (
+						<Text.Base linkify>{title}</Text.Base>
+					) : (
+						<Input.Text
+							placeholder={t('eventTitle')}
+							value={title}
+							onChange={onTitleChange}
+							maxLength="140"
+						/>
+					)}
+				</div>
+				<div
+					className="time-info"
+					hidden={suppressDuplicateInfoFromCustomHeader}
+				>
+					<DateTime
+						className="date"
+						date={startDate}
+						format={DateTime.WEEKDAY_AT_TIME_PADDED_WITH_ZONE}
 					/>
-				)}
-			</div>
-			<div className="time-info">
-				<DateTime
-					className="date"
-					date={startDate}
-					format={DateTime.WEEKDAY_AT_TIME_PADDED_WITH_ZONE}
-				/>
-			</div>
-			<div className="image-and-description">
-				{readOnly ? (
-					img && <img className="preview" src={img.src} />
-				) : (
-					<ImageUpload img={img} onChange={onImageChange} />
-				)}
-				{readOnly ? (
-					<Text.Base className="desc" linkify>
-						{description}
-					</Text.Base>
-				) : (
-					<Input.TextArea
-						value={description}
-						onChange={onDescriptionChange}
-						placeholder={t('eventDescription')}
-					/>
-				)}
+				</div>
+				<div className="image-and-description">
+					{readOnly ? (
+						img && <img className="preview" src={img.src} />
+					) : (
+						<ImageUpload img={img} onChange={onImageChange} />
+					)}
+					{readOnly ? (
+						<Text.Base className="desc" linkify>
+							{description}
+						</Text.Base>
+					) : (
+						<Input.TextArea
+							value={description}
+							onChange={onDescriptionChange}
+							placeholder={t('eventDescription')}
+						/>
+					)}
+				</div>
 			</div>
 		</div>
 	);
 }
 
-function DateInfo({ date }) {
+function DateInfo({ date, ...props }) {
 	return (
-		<div className="date">
+		<div className="date" {...props}>
 			<DateTime
 				as="div"
 				className="month"
