1342ecbbaf8fd73074a8d8973e7c6ade243c25db
Jonathan Grimes <jsg2021@gmail.com>
2021-07-21 13:50:28 -0500

    Add TODO prolog

--- src/events/View.jsx
+++ src/events/View.jsx
@@ -21,6 +21,20 @@ View.defaultProps = {
 	dialog: true,
 };
 
+/*
+TODO: Refactor into discrete Frames, Viewer, and Editor
+The structure of this is a mess.
+The viewer is all three: viewer, editor, AND frame. Each mode conflicting with one another on
+what they expect when used in various contexts.
+
+The solution is to extract each "mode" and their containers to be independent components.
+I've started the process, but preserved the original structures. The complex class is now
+exploded into small parts, that need further exploding to isolate the "view" mode bits from
+the "editor" mode bits. The containers and layout parts should be shareable. However, there
+needs to be discrete entry for "view" and "edit" with the option of using any "Frame" needed
+but whatever context we throw at it.
+*/
+
 export function View(props) {
 	const { event, dialog, editable } = props;
 
