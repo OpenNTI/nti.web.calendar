8abb993b42786bdc192b7ab0cdc54ab12f04f02a
Jonathan Grimes <jsg2021@gmail.com>
2021-09-07 15:25:27 -0500

    NTI-11103 - disable interactivity if no calendar data exposed

--- src/widgets/DateIcon.jsx
+++ src/widgets/DateIcon.jsx
@@ -1,16 +1,31 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import PropTypes from 'prop-types';
 
 import { DateTime } from '@nti/web-commons';
+import { useService } from '@nti/web-core';
 
 import Store from './DateIconStore';
 
 const Wrapper = styled.div`
 	cursor: pointer;
+
+	&.disabled {
+		opacity: 0.5;
+		pointer-events: none;
+	}
 `;
 
 const DateIcon = React.forwardRef((props, ref) => {
 	const { todaysCount, markSeen, hasSeen } = Store.useValue();
+	const available = !useService()
+		.getCollection('Calendars')
+		?.getLink('events');
+	const disabled = available
+		? null
+		: {
+				disabled: true,
+				onClick: null,
+		  };
 
 	return (
 		<Wrapper
@@ -19,9 +34,11 @@ const DateIcon = React.forwardRef((props, ref) => {
 			onClick={() => {
 				markSeen();
 			}}
+			{...disabled}
 		>
 			<DateTime.DateIcon
 				{...props}
+				{...disabled}
 				viewed={hasSeen}
 				badge={todaysCount || 0}
 			/>
@@ -33,4 +50,8 @@ DateIcon.propTypes = {
 	date: PropTypes.object,
 };
 
-export default Store.compose(DateIcon);
+export default Store.compose(props => (
+	<Suspense fallback={<div />}>
+		<DateIcon {...props} />
+	</Suspense>
+));
